93b44c1f4db284599af23a1fb18dbb675f1baed6
phamLinh98 <linhthusinh98@gmail.com>
2024-12-24 18:30:28 +0530

    fix bug node module

--- src/api/restApiConfig.js
+++ src/api/restApiConfig.js
@@ -1,15 +1,19 @@
 import { envConfig } from "../configs/envConfig";
 
 export const getApi = async (route) => {
-    const url = `${envConfig.host}${route}`;
-    const response = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
+    try {
+        const url = `${envConfig.host}${route}`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status:${response.status}`);
         }
-    });
-    if (!response.ok) {
-        throw new Error(`HTTP error! status:${response.status}`);
+        return response;
+    } catch (error) {
+        console.log('error', error)
     }
-    return response;
 }
